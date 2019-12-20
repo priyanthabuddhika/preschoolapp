f60509373e9d526be3c3f58f3d791f7317704428
Priyantha Buddhika <priyanthabuddhika@gmail.com>
2019-12-20 23:41:49 +0530

    Successfully migrated to AndroidX

--- preschool_app/lib/screens/screens/home/home.dart
+++ preschool_app/lib/screens/screens/home/home.dart
@@ -5,22 +5,70 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _auth = AuthService();
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Pre School Arcade'),
-        backgroundColor: Colors.white70,
-        actions: <Widget>[
-          FlatButton.icon(
-            label: Text('Sign Out'),
-            icon: Icon(Icons.person),
-            onPressed: () async {
-              await _auth.signingOut();
-              
-            },
-          )
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(15.0),
+            color: Colors.blueAccent[200],
+            child: ListTile(
+              title: Text('Priyantha'),
+              subtitle: Text('abc@abc.com'),
+              trailing: CircleAvatar(
+                radius: 20.0,
+                backgroundImage: AssetImage('images/profile.png'),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            padding: EdgeInsets.fromLTRB(0, 50, 20, 0),
+            color: Colors.cyanAccent,
+            child: Text('Welcome To Parent Dashboard'),
+          ),
         ],
       ),
     );
   }
 }
+
+/* appBar: AppBar(
+          title: Text('Pre School Arcade'),
+          backgroundColor: Colors.white70,
+          actions: <Widget>[
+            FlatButton.icon(
+              label: Text('Sign Out'),
+              icon: Icon(Icons.person),
+              onPressed: () async {
+                await _auth.signingOut();
+                
+              },
+            )
+          ],
+        ),
+
+
+
+       
+
+
+
+       */
